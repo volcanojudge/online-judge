4fe13d4186ba0135e4c6460078164e110a4214e0
Crator Judge <crator@judge.ca>
2023-01-02 17:53:55 +0000

    fix errors

--- src/csandbox.c
+++ src/csandbox.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <seccomp.h>
 
-void __attribute__ ((constructor)) install_syscall_filter() {
+void __attribute__ ((constructor (0))) install_syscall_filter() {
   scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_KILL);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 0); // FROM 0
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(open), -1);
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO(1), SCMP_SYS(open), 1);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(close), 0);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(stat), 0);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(fstat), 0);
@@ -17,12 +17,12 @@ void __attribute__ ((constructor)) install_syscall_filter() {
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(pause), 0); // FROM 34
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(nanosleep), 0); // TO 35
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0); // 60
-  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getuid), -1); // FROM 102
-  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getgid), -1);
-  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(geteuid), -1);
-  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getegid), -1);
-  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getppid), -1);
-  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getpgrp), -1); // TO 111
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(getuid), 0); // FROM 102
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(getgid), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(geteuid), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(getegid), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO(1), SCMP_SYS(getppid), 1);
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO(1), SCMP_SYS(getpgrp), 1); // TO 111
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(futex), 0); // 202
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit_group), 0); // 231
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(newfstatat), 0); // 262
