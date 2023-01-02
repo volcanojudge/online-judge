c9d4515458cc4c320001ec47e1e3805757e75de0
Jonathan Wu <jonathan.wu3@outlook.com>
2023-01-02 00:28:20 -0500

    more seccomps why are there so many syscalls

--- src/csandbox.c
+++ src/csandbox.c
@@ -3,19 +3,30 @@
 
 void __attribute__ ((constructor)) install_syscall_filter() {
   scmp_filter_ctx ctx = seccomp_init(SCMP_ACT_KILL);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit_group), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 0); // FROM 0
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(getrandom), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(brk), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(mmap), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(munmap), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(open), -1);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(close), 0);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(stat), 0);
   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(fstat), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(lstat), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(newfstatat), 0);
-  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(futex), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(lstat), 0); // TO 7
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(mmap), 0); // FROM 9
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(mprotect), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(munmap), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(brk), 0); // TO 12
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(pause), 0); // FROM 34
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(nanosleep), 0); // TO 35
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0); // 60
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getuid), -1); // FROM 102
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getgid), -1);
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(geteuid), -1);
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getegid), -1);
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getppid), -1);
+  seccomp_rule_add(ctx, SCMP_ACT_ERRNO, SCMP_SYS(getpgrp), -1); // TO 111
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(futex), 0); // 202
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit_group), 0); // 231
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(newfstatat), 0); // 262
+  seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(getrandom), 0); // FROM 317 TO 318
   seccomp_load(ctx);
   seccomp_release(ctx);
 }
